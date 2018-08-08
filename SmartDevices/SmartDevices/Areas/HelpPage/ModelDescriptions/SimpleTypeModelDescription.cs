5ebb07cec611bdf0e3219eae77d676a08ba10706
Wahib Thabet <wthabet@gmail.com>
2018-08-08 15:10:32 -0400

    Initial commit

---
+++ SmartDevices/SmartDevices/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SmartDevices.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SmartDevices/SmartDevices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartDevices.WebApiApplication" Language="C#" %>
